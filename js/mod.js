7823d3a11e5cdf0a6a2500bed78a2d6a33655c00
Jumpie6969 <137106978+Jumpie6969@users.noreply.github.com>
2024-04-27 20:22:47 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Points Tree",
+	id: "tptv0p1",
+	author: "Jumpie6969",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
